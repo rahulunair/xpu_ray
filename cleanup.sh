25b32e5cb86c7b8ebebd76c3866ee899360c7d34
Rahul <rahul.unnikrishnan.nair@intel.com>
2024-12-04 10:09:20 -0800

    fix

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,5 @@
 docker compose down --remove-orphans
-docker rmi xpu_ray-sd-service xpu_ray-auth xpu_ray-traefik
+docker rmi xpu_ray-sd-service xpu_ray-auth xpu_ray-traefik 2>/dev/null || true
 docker volume prune -f
 docker network prune -f
 docker image prune -f
